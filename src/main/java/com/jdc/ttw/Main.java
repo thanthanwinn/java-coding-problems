0dd49c42d64c248a6175f9f08526cefa91a6db8e
thanthanwin <thanthanwinn27@gmail.com>
2025-01-14 19:42:29 +0630

    counting duplicate characters

---
+++ src/main/java/com/jdc/ttw/Main.java
@@ -0,0 +1,37 @@
+package com.jdc.ttw;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		countChar("hello world", 'o');
+	}
+	
+	public  static void countChar(String str, char c) {
+		char[] arr = new char[str.length()];
+
+		for(int i=0; i < str.length(); i++) {
+			arr[i] = str.charAt(i);
+		}
+		
+		int count = 0;
+		for(int i = 0; i< arr.length; i++) {
+			
+				if (c == arr[i]) {
+					count++;
+				
+			}
+		}
+		if(c == ' ') {
+			System.out.print("Space character contains in string  "+ str +" for "+ count + " time");
+		}else {
+		System.out.print( c + " character contains in string  "+ str +" for "+ count + " time");
+		}
+		if (count > 1) {
+			System.out.println("s");
+		}
+	
+	}
+
+}
