f24e08ad4964076d1d8f5cde46b40a6f67531720
thanthanwin <thanthanwinn27@gmail.com>
2025-01-20 22:47:16 +0630

    countingVowelsAndConsonents

---
+++ src/main/java/com/jdc/ttw/CountingVowelsAndConsonents.java
@@ -0,0 +1,24 @@
+package com.jdc.ttw;
+
+public class CountingVowelsAndConsonents {
+	public static void main(String[] args) {
+		count("Helloo");
+	}
+	
+	public static void count(String str) {
+		str = str.toLowerCase();
+		int vowels = 0;
+		int consonents = 0;
+		for(int i = 0; i<str.length();i++) {
+			char c = str.charAt(i);
+			if("aeiou".contains(String.valueOf(str.charAt(i)))) {
+				vowels++;
+			}else if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+				consonents++;
+			}
+		}
+		System.out.println("Vowels: " + vowels);
+		System.out.println("Consonents: " + consonents);
+	}
+
+}
